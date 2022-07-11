b6910b787f8d21624c34d333bf8f7d7d46bbef51
Nyfaria DragonMoose <kinaria3324@gmail.com>
2022-07-11 09:59:08 -0500

    Delete build directory

--- build/tmp/expandedArchives/forge-1.19-41.0.63_mapped_official_1.19-sources.jar_2ee6f91171d97bd4c2b0b750e723b13b/net/minecraft/commands/arguments/SignedArgument.java
+++
@@ -1,7 +0,0 @@
-package net.minecraft.commands.arguments;
-
-import net.minecraft.network.chat.Component;
-
-public interface SignedArgument<T> extends PreviewedArgument<T> {
-   Component getPlainSignableComponent(T p_233887_);
-}
